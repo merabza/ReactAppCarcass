4540257fde81fbc94a8269fdf816cf0326b443ea
merabza <merabza@gmail.com>
2024-01-10 13:48:21 +0400

    202401101348

--- grid/PosibleValuesFilterComboBox.tsx
+++ grid/PosibleValuesFilterComboBox.tsx
@@ -127,7 +127,10 @@ const PosibleValuesFilterComboBox: FC<PosibleValuesFilterComboBoxProps> = (
             onChangeValue(undefined);
           }}
         >
-          <FontAwesomeIcon icon="minus" />
+          {/* <FontAwesomeIcon icon="minus" /> */}
+          <svg width="14" height="14" viewBox="0 0 14 14">
+            <path d="M13.222 2H.778C.348 2 0 1.552 0 1s.348-1 .778-1h12.444c.43 0 .778.448.778 1s-.348 1-.778 1zM1.556 3.111l3.888 4.667v5.444c0 .43.349.778.778.778h1.556c.43 0 .778-.348.778-.778V7.778l3.888-4.667H1.556z"></path>
+          </svg>{" "}
         </Button>
       </Col>
     </Form.Group>
